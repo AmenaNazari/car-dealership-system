40b9602b6cd0a2225ba82e99cdd00fba7991197c
Amena Nazari <nazara@jevs.org>
2025-05-17 07:04:22 -0400

    complet Contract class

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicle;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
+    }
+
+    // Getters
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    // Abstract methods
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
+
+
